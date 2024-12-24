3080aab0a77c695409d178be258d59ee8c535af7
Artemis <falletkamagate3@gmail.com>
2024-12-24 23:57:12 +0100

    fallet update

--- models/loginModels.js
+++ models/loginModels.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
 
 const LoginSchema = mongoose.Schema({
     username: { type: String, required: true },
     password:{ type: String, required: true },
 });
-
-LoginSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Login', LoginSchema);
